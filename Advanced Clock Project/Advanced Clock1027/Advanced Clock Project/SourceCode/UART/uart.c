497b5694763c6e8554f2272dbc8a09748230fc25
ZhangYixiSucceed <guoqingzhang0813@163.com>
2022-11-29 21:39:19 +0800

    update bluetooth print

--- Advanced Clock Project/Advanced Clock1027/Advanced Clock Project/SourceCode/UART/uart.c
+++ Advanced Clock Project/Advanced Clock1027/Advanced Clock Project/SourceCode/UART/uart.c
@@ -360,7 +360,7 @@ void UART2Init(void)
     GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_3;   
     GPIO_Init(GPIOA, &GPIO_InitStructure); 
 
-    USART_InitStructure.USART_BaudRate            = 115200;
+    USART_InitStructure.USART_BaudRate            = 9600;
     USART_InitStructure.USART_WordLength          = USART_WordLength_8b;
     USART_InitStructure.USART_StopBits            = USART_StopBits_1;     
     USART_InitStructure.USART_Parity              = USART_Parity_No;
