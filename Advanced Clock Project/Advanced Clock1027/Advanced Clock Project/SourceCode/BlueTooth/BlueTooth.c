83e52759a005c068669b8287489ed1ae42f7fa49
ZhangYixiSucceed <guoqingzhang0813@163.com>
2022-12-14 00:00:34 +0800

    update ble

--- Advanced Clock Project/Advanced Clock1027/Advanced Clock Project/SourceCode/BlueTooth/BlueTooth.c
+++ Advanced Clock Project/Advanced Clock1027/Advanced Clock Project/SourceCode/BlueTooth/BlueTooth.c
@@ -1,11 +1,35 @@
 #include "main.h"
 
+void BleStateCheck(char *data)
+{
+	unsigned char *point;
+    point = strstr(data, "GetWifi");               // IP_CONNECT 
+    if(point != NULL)
+    {
+      return RESP_BLE_GET_WIFI_INFO;
+    }
+}
+
 void bluetooth_msg_porcess()
 {
+  unsigned char mrtn;
   if(queue_out(&ble_queue, FrameInBuff, &FrameInlen) == QUEUE_OPER_OK) 
   {
-    UART2_SentMsgL(FrameInBuff,FrameInlen);
-    memset(FrameInBuff,0x00,sizeof(FrameInBuff));
+	  memset(&FrameInBuff[FrameInlen],0x00,RX_BUFFER_SIZE - FrameInlen); 
+	  UART2_SentMsgL(FrameInBuff,FrameInlen);
+	  switch(sOperCmdBuff.tid)
+	  {
+	  		 case AT_CMD:                                                                 // 2�����CESQ
+		      mrtn = BleStateCheck((char*)FrameInBuff);
+		      if(mrtn == RESP_WIFI_OK)
+		      {                                                                     // recv ok
+		          sOperCmdBuff.tid = 0xff;
+
+		      }
+		      break;
+			  default:
+			  	break;
+	  }
   }
 }
 
--- Advanced Clock Project/Advanced Clock1027/Advanced Clock Project/SourceCode/BlueTooth/BlueTooth.h
+++ Advanced Clock Project/Advanced Clock1027/Advanced Clock Project/SourceCode/BlueTooth/BlueTooth.h
@@ -1,7 +1,11 @@
 #ifndef  __BLUETOOTH_H__
 #define  __BLUETOOTH_H__
 
+enum {
+	RESP_BLE_GET_WIFI_INFO  = 0x00,
+	
 
+};
 
 void bluetooth_msg_porcess();
 
