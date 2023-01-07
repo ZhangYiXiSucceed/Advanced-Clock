49bcf3659d6e8315eff7f432aaf39a93f67287c6
ZhangYixiSucceed <guoqingzhang0813@163.com>
2023-01-07 22:55:52 +0800

    update boot

---
+++ Advanced Clock Project/Advanced Clock1027/Advanced Clock Project/SourceCode/bootloader/boot_main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+int main()
+{
+  u8 Data[32]="go to while!\r\n";
+  
+  SysTick_Config(SystemCoreClock/1000000);
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+  shell_init();
+  
+  TIM5_Init();
+  
+  My_RTC_Init();
+  
+  init_wifi_network();
+  IWDG_Init();
+
+  rt_kprintf((char*)Data);
+  rt_kprintf2((char*)Data);
+
+  while(1)
+  {
+    IWDG_Feed();
+    shell_process();
+	periodic_task_process();
+    wifi_msg_process();
+
+  }
+
---
+++ Advanced Clock Project/Advanced Clock1027/Advanced Clock Project/SourceCode/bootloader/boot_main.h
@@ -0,0 +1,24 @@
+#ifndef  __BOOT_MAIN_H__
+#define  __BOOT_MAIN_H__
+
+#include "core_cm4.h"
+#include "stm32f4xx_rtc.h"
+#include "stm32f4xx.h"
+#include "stm32f4xx_it.h"
+#include "timer.h"
+#include "uart.h"
+#include "queue.h"
+#include "msg_deal.h"
+#include "iwdg.h"
+#include "common.h"
+#include "WIFI.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "struct.h"
+#include "math.h"
+
+
+#endif
+
